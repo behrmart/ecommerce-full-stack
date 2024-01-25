22096bcbd388471681d8d6f5814a8b97729c2bed
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2024-01-25 13:23:38 -0600

    Center cards with Box

--- components/Products.js
+++ components/Products.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { data } from '../utils/data'
-import { Image } from '@chakra-ui/react'
+import { Heading, Image } from '@chakra-ui/react'
 import { Grid, GridItem, Card, Box} from '@chakra-ui/react'
 import Link from 'next/link'
 
 const Products = () => {
   return (
     <div>
+        <Box display="flex" alignItems="center" justifyContent="space-between">
         <Grid templateColumns={{base: "1fr", lg: "repeat(4, 1fr)"}} gap={6}>
            {data.products.map((products) => (
             <GridItem key={products.id} colSpan={1}>
@@ -18,17 +19,19 @@ const Products = () => {
                 width={230}
                 style={{objectFit: "cover" , height:"230px"}}
                 alt={products.title}
+                borderRadius='lg'
                />  
                <Box py={1} px={3} >
-                <h3>{products.title}</h3>
+                <Heading size='md'>{products.title}</Heading>
                 <div>{products.description}</div>
-                <div>{products.price}</div>
+                <div>Price: ${products.price}</div>
                </Box>
                </Link>
                 </Card>
             </GridItem>
         ))}
         </Grid>
+</Box>
     </div>
   )
 }
