6dfdad1660537fdae4d47da2f94f3c3e6f274bba
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2024-01-25 12:16:57 -0600

    Fixed class code

---
+++ utils/data.js
@@ -0,0 +1,56 @@
+//data dummy for products
+
+export const data = {
+    products: [
+        {
+            id:1,
+            title: 'Product 1',
+            description: 'Product 1 description',
+            price: 100,
+            image: '/23.jpg'
+        },
+        {
+            id:1,
+            title: 'Product 2',
+            description: 'Product 2 description',
+            price: 200,
+            image: '/46.jpg'
+        },
+        {
+            id:1,
+            title: 'Product 3',
+            description: 'Product 3 description',
+            price: 300,
+            image: '/72.jpg'
+        },
+        {
+            id:1,
+            title: 'Product 4',
+            description: 'Product 4 description',
+            price: 400,
+            image: '/73.jpg'
+        },
+        {
+            id:1,
+            title: 'Product 5',
+            description: 'Product 5 description',
+            price: 500,
+            image: '/74.jpg'
+        },
+        {
+            id:1,
+            title: 'Product 6',
+            description: 'Product 6 description',
+            price: 600,
+            image: '/75.jpg'
+        },
+        {
+            id:1,
+            title: 'Product 7',
+            description: 'Product 7 description',
+            price: 700,
+            image: '/76.jpg'
+        },
+
+    ]
+}
